0535c3dced48cc06fef6df2e70cc82b544937065
Anastasia <chestaci@gmail.com>
2023-04-02 00:03:40 +0300

    Corrections have been made.

--- src/test/java/com/github/Chestaci/CustomerSearchTest.java
+++ src/test/java/com/github/Chestaci/CustomerSearchTest.java
@@ -101,6 +101,12 @@ public class CustomerSearchTest {
 
         listCustomerPage.inputSearchCustomer(searchCustomer);
 
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         //Получение списока строк в таблице с клиентами после
         //проведения поиска по заданным параметрам
         List<WebTableElement> customersList = listCustomerPage.getTableCustomerList().getListElement();
